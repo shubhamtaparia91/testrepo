35df40fc320ce9e079d316c9766e2159a258fda6
shubhamtaparia91 <125902367+shubhamtaparia91@users.noreply.github.com>
2023-02-20 21:25:22 +0530

    Add files via upload

---
+++ AdminPageAfterLogin.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Admin_Username"] == null)
+        {
+            Response.Redirect("Start_Page.aspx");
+        }
+
+    }
+    protected void LinkButton10_Click(object sender, EventArgs e)
+    {
+        Session["Admin_Username"] = null;
+        Response.Redirect("Start_Page.aspx");
+
+    }
+}
